2f3bfae338af8fbcd4caf10ee4893da7e7d31374
juju4 <juju4@users.noreply.github.com>
2019-04-13 20:44:39 -0400

    Revert "ansible lint" and most following changes
    This reverts commit 4ba129ba4ab53e7c186a5dde8d72b852fd7dd57a.

--- templates/sumo-env.sh
+++ templates/sumo-env.sh
@@ -8,7 +8,7 @@ SUMO_ACCESS_ID="{{ sumologic_collector_accessid }}"
 SUMO_ACCESS_KEY="{{ sumologic_collector_accesskey }}"
 # also defined in /etc/sumo/sumo.conf
 SUMO_COLLECTOR_NAME={{ inventory_hostname }}
-{% if sumologic_single_source_file %}
+{% if sumologic_single_source_file == "yes" %}
 SUMO_SOURCES_JSON=/etc/sumo/sumologic-collector.json
 {% else %}
 SUMO_SOURCES_JSON=/etc/sumo/sumo.d 
